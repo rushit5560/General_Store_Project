7a4857d43478ac765723c83ec75c0463f514f161
Rushit <90950237+rushit5560@users.noreply.github.com>
2021-12-06 16:40:37 +0530

    Collection Screen Api

--- general_store/lib/screens/collection_screen/collection_screen.dart
+++ general_store/lib/screens/collection_screen/collection_screen.dart
@@ -1,38 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:general_store/common/img_url.dart';
-import 'package:general_store/screens/product_detail_screen/product_detail_screen.dart';
+import 'package:general_store/common/custom_widget.dart';
+import 'package:general_store/controllers/collection_screen_controller/collection_screen_controller.dart';
 import 'package:get/get.dart';
-import '../../models/collection_screen_model/collection_model.dart';
+import 'collection_screen_widgets.dart';
 
 class CollectionScreen extends StatelessWidget {
   // const CollectionPage({Key? key}) : super(key: key);
+  final collectionScreenController = Get.put(CollectionScreenController());
 
-  List<CollectionSingleItem> collectionLists = [
-    CollectionSingleItem(
-      collectionImg: ImgUrl.popular1, collectionName: 'Collection Name 1',
-    ),
-    CollectionSingleItem(
-      collectionImg: ImgUrl.popular2, collectionName: 'Collection Name 2',
-    ),
-    CollectionSingleItem(
-      collectionImg: ImgUrl.popular3, collectionName: 'Collection Name 3',
-    ),
-    CollectionSingleItem(
-      collectionImg: ImgUrl.popular4, collectionName: 'Collection Name 4',
-    ),
-    CollectionSingleItem(
-      collectionImg: ImgUrl.popular1, collectionName: 'Collection Name 1',
-    ),
-    CollectionSingleItem(
-      collectionImg: ImgUrl.popular2, collectionName: 'Collection Name 2',
-    ),
-    CollectionSingleItem(
-      collectionImg: ImgUrl.popular3, collectionName: 'Collection Name 3',
-    ),
-    CollectionSingleItem(
-      collectionImg: ImgUrl.popular4, collectionName: 'Collection Name 4',
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -46,63 +21,13 @@ class CollectionScreen extends StatelessWidget {
         ),
       ),
 
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: GridView.builder(
-          physics: BouncingScrollPhysics(),
-          itemCount: collectionLists.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 10, crossAxisSpacing: 10,
-            childAspectRatio: 0.9
-          ),
-          itemBuilder: (context, i){
-            return GestureDetector(
-              onTap: () {
-                // print(collectionLists[i].collectionName);
-                Get.to(()=> ProductDetailScreen());
-              },
-              child: Stack(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image: AssetImage(collectionLists[i].collectionImg),
-                        fit: BoxFit.cover
-                      )
-                    ),
-                  ),
-
-                  Positioned(
-                    bottom: 15,
-                    child: Container(
-                      width: Get.width * 0.38,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 10, top: 3, bottom: 3),
-                        child: Text(
-                          collectionLists[i].collectionName,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 15
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+      body: Obx(
+        () => collectionScreenController.isLoading.value
+            ? CustomCircularProgressIndicator()
+            : Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: CollectionListModule(),
               ),
-            );
-          },
-        ),
       ),
     );
   }
---
+++ general_store/lib/screens/collection_screen/collection_screen_widgets.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:general_store/common/api_url.dart';
+import 'package:general_store/controllers/collection_screen_controller/collection_screen_controller.dart';
+import 'package:general_store/models/collection_screen_model/collection_model.dart';
+import 'package:general_store/screens/product_detail_screen/product_detail_screen.dart';
+import 'package:get/get.dart';
+
+class CollectionListModule extends StatelessWidget {
+  CollectionListModule({Key? key}) : super(key: key);
+  final collectionScreenController = Get.find<CollectionScreenController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      physics: BouncingScrollPhysics(),
+      itemCount: collectionScreenController.collectionLists.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10, crossAxisSpacing: 10,
+          childAspectRatio: 0.9
+      ),
+      itemBuilder: (context, i){
+        Datum1 collectionSingleItem = collectionScreenController.collectionLists[i];
+        return collectionListTile(collectionSingleItem);
+      },
+    );
+  }
+
+  Widget collectionListTile(Datum1 collectionSingleItem) {
+    String imgUrl = ApiUrl.ApiMainPath + collectionSingleItem.showimg;
+    return GestureDetector(
+      onTap: () {
+        Get.to(()=> ProductDetailScreen());
+      },
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                    image: NetworkImage("$imgUrl"),
+                    fit: BoxFit.cover
+                )
+            ),
+          ),
+
+          Positioned(
+            bottom: 15,
+            child: Container(
+              width: Get.width * 0.38,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, top: 3, bottom: 3),
+                child: Text(
+                  collectionSingleItem.productname,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                      fontSize: 15
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
